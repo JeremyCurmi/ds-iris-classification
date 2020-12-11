0a8950688f14a27cffe87c0e433715c2b3c1d476
DataScienceXp <54748653+DataScienceXp@users.noreply.github.com>
2020-12-11 15:22:42 +0100

    app v1 ready

---
+++ app/flask_api_swagger.py
@@ -0,0 +1,86 @@
+from flask import Flask, request
+
+import joblib
+import pandas as pd
+import numpy as np
+
+import flasgger
+from flasgger import Swagger
+
+app=Flask(__name__) # from which point you want to start the app
+Swagger(app)
+
+joblib_in = open('model.pkl','rb')
+classifier = joblib.load(joblib_in)
+
+@app.route('/') # decorater # this is my route api
+def welcome():
+    return "Welcome All"
+
+@app.route('/predict', methods=["GET"]) # when method is not specified, automatically its set as GET
+def predict_iris_species():
+    """
+     Let's Classify Iris Species
+    ---
+    parameters:
+        - name: sepal_length
+          in: query
+          type: number
+          required: true
+        - name: sepal_width
+          in: query
+          type: number
+          required: true
+        - name: petal_length
+          in: query
+          type: number
+          required: true
+        - name: petal_width
+          in: query
+          type: number
+          required: true
+    responses:
+        200:
+            description: The output values
+            
+    """
+    sepal_length = float(request.args.get('sepal_length'))
+    sepal_width = float(request.args.get('sepal_width'))
+    petal_length = float(request.args.get('petal_length'))
+    petal_width = float(request.args.get('petal_width'))
+
+    print(f"sepal_length: {sepal_length}")
+    print(f"sepal_width: {sepal_width}")
+    print(f"petal_length: {petal_length}")
+    print(f"petal_width: {petal_width}")
+    prediction = classifier.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+
+    return f"The predicted value is {prediction}"
+
+@app.route('/predict_file', methods=["POST"]) # Post was required as we need to give in a file
+def predict_file_iris_species():
+    """
+    Let's Classify Iris Species using csv data
+    ---
+    parameters:
+        - name: file
+          in: formData
+          type: file
+          required: true
+
+    responses:
+        200:
+            description: The output values
+    """
+    print("loading data file ...")
+    df = pd.read_csv(request.files.get("file"))
+    prediction = classifier.predict(df)
+
+    return f"The predicted value for the csv {list(prediction)}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000)
+
+
+
